ec3fbdc73dd6a9519045c9c3ec121ceeeea3d032
kjs <135118163+novellus1013@users.noreply.github.com>
2024-08-11 20:53:34 +0900

    [level 1] Title: 과일 장수, Time: 87.19 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,3 +1,17 @@
 def solution(k, m, score):
+    # 1
+    score.sort(reverse=True)
+    
+    # 2
     answer = 0
+    for i in range(0, len(score) - m + 1, m):
+        # 3
+        answer += score[i + m - 1] * m
+    
     return answer
+
+
+
+# 1. score를 내림차순으로 정렬한다.
+# 2. score에서부터 m개씩 원소를 가지는 상자들을 구한다.
+# 3. 상자들을 순회화면서 각 상자의 가장 마지막 항 x m 을 구해 answer에 더한다. 
