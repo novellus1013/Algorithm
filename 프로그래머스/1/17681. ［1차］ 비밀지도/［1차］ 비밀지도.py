f1dd5e13d6078ddd6caa6992ff2157bdbfbbf94b
kjs <135118163+novellus1013@users.noreply.github.com>
2024-08-23 17:29:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,25 @@
+def solution(n, arr1, arr2):
+    answer = []
+    
+    # 2진수로 변환
+    arr1 = [bin(i)[2:] for i in arr1]
+    arr2 = [bin(i)[2:] for i in arr2]
+    
+    # n의 길이만큼 모든 값이 벽과 공백을 나타내도록 0을 가지게 변환 (조건3)
+    arr1 = ["0" * (n - len(i)) + i if len(i) != n else i for i in arr1]
+    arr2 = ["0" * (n - len(i)) + i if len(i) != n else i for i in arr2]
+            
+    
+    for i in range(0, n):
+        maps = ""
+        
+        for y in range(0, n):
+            
+            if arr1[i][y] == "0" and arr2[i][y] == "0":
+                maps += " "
+            else:
+                maps += "#"
+            
+        answer.append(maps)
+                
+    return answer
