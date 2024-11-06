7fca9334ad747ce1927682d50dc6c2acfc355719
kjs <135118163+novellus1013@users.noreply.github.com>
2024-11-06 21:30:32 +0900

    [level 3] Title: 여행경로, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,23 +1,21 @@
-from collections import deque
-
 def solution(tickets):
     tickets.sort()
     answer = []
     visited = [False] * len(tickets)
-    q = deque([("ICN", ["ICN"], visited)])
+    stack = [("ICN", ["ICN"], visited)]
 
-    while q:
-        current, path, visited_state = q.popleft()
+    while stack:
+        current, path, visited_state = stack.pop()
         
         if len(path) == len(tickets) + 1:
             answer = path
             break
         
-        for i in range(len(tickets)):
+        for i in range(len(tickets) -1, -1, -1):
             if not visited_state[i] and tickets[i][0] == current:
                 new_visited = visited_state[:]
                 new_visited[i] = True
-                q.append((tickets[i][1], path + [tickets[i][1]], new_visited))
+                stack.append((tickets[i][1], path + [tickets[i][1]], new_visited))
                 
     return answer
 
