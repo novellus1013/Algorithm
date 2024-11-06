2bab063c4c0041bcb0e3cfef50ba77bde762c93c
kjs <135118163+novellus1013@users.noreply.github.com>
2024-11-07 01:42:10 +0900

    [level 2] Title: 전화번호 목록, Time: 102.83 ms, Memory: 30.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(phone_book):
+    phone_book.sort();
+    
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1][0:len(phone_book[i])] == phone_book[i]:
+            return False
+    
+    
+    return True
