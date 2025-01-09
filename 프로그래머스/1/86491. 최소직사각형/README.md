f64155579d24aaf33c1d171e0dfd195413e39908
kjs <135118163+novellus1013@users.noreply.github.com>
2025-01-09 11:58:10 +0900

    [level 1] Title: 최소직사각형, Time: 5.34 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,6 +1,6 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 09일 11:57:39
+2025년 01월 09일 11:58:08
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,19 +1,3 @@
 def solution(sizes):
     answer = 0
-    big = []
-    small = []
-    
-    for i in sizes:
-        if i[0] >= i[1]:
-            big.append(i[0])
-            small.append(i[1])
-        else:
-            big.append(i[1])
-            small.append(i[0])
-    
-    big.sort(reverse = True)
-    small.sort(reverse = True)
-    
-    answer = big[0] * small[0]
-    
     return answer
